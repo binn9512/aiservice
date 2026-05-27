fe1e4dbca5fb61e1a3376c86b1d4d90b5d9b5f72
‪♡‬ <seoha@seohaui-MacBookPro.local>
2026-05-28 00:21:59 +0900

    merge develop and organize backend structure

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-from vision_part import analyze_style
-import db_test
+from backend.vision_part import analyze_style
+import backend.db_test as db_test
 
 def process_and_save(user_id, image_path):
 
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     process_and_save("user1", "blouse2.png")
     process_and_save("user1", "pant.png")
     process_and_save("user1", "skirt.png")
+    process_and_save("user1", "greenblouse.png")
     db_test.check_data()
     # # 이미지 경로를 여기에 입력하세요
     # image_path = "jacket.png" 
